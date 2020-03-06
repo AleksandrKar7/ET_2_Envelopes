b1aeac9d9370a722c4241b65a46f6026e32763c9
AleksandrKar7 <AleksandrKar7@gmail.com>
2020-03-06 08:27:50 +0200

    Initial commit

---
+++ ET_2_Envelopes/ConsoleMenu.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET_2_Envelopes
+{
+    static class ConsoleMenu
+    {
+        public static double? GetDoubleValue(string message)
+        {
+            double result;
+            string text;
+            
+            Console.WriteLine(message);
+            text = Console.ReadLine();
+
+            if(Double.TryParse(text, out result))
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool? GetBoolValue(string message)
+        {
+            string text;
+            string insturction;
+            string[] trueArray = { "T", "TRUE" };
+            string[] falseArray = { "F", "FALSE" };
+
+            insturction = String.Format("For true: {0}; For false: '{1}'", 
+                String.Join("', ", trueArray), String.Join("', ", falseArray));
+
+            Console.WriteLine(message);
+            Console.WriteLine(insturction);
+
+            text = Console.ReadLine();
+            text.Trim();
+            text = text.ToUpper();
+
+            if(text == null)
+            {
+                return null;
+            }
+
+            if (trueArray.Contains(text))
+            {
+                return true;
+            }
+            if (falseArray.Contains(text))
+            {
+                return false;
+            }
+
+            return null;
+        }
+
+        public static bool GetBoolValue(string message, string[] trueArray)
+        {
+            if (trueArray == null)
+            {
+                return false;
+            }
+            string text;
+            string insturction;
+
+
+            insturction = String.Format("For true: {0}; For false: '{1}'",
+                String.Join("', ", trueArray), "Press enter");
+
+            Console.WriteLine(message);
+            Console.WriteLine(insturction);
+
+            text = Console.ReadLine();
+            text.Trim();
+            text = text.ToUpper();
+
+            if (text == null)
+            {
+                return false;
+            }
+
+            if (trueArray.Contains(text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool? GetBoolValue(string message, string[] trueArray, string[] falseArray)
+        {
+            if (trueArray == null)
+            {
+                return null;
+            }
+            if (falseArray == null)
+            {
+                return null;
+            }
+            string text;
+            string insturction;
+
+            insturction = String.Format("For agree: {0}; For disagree: '{1}'",
+                String.Join("', ", trueArray), String.Join("', ", falseArray));
+
+            Console.WriteLine(message);
+            Console.WriteLine(insturction);
+
+            text = Console.ReadLine();
+            text.Trim();
+            text = text.ToUpper();
+
+            if (text == null)
+            {
+                return null;
+            }
+            if (trueArray.Contains(text))
+            {
+                return true;
+            }
+            if (falseArray.Contains(text))
+            {
+                return false;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ET_2_Envelopes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET_2_Envelopes
+{
+    class Program
+    {
+        const int MaxCountEnvelope = 2;
+        static void Main(string[] args)
+        {
+            IEnvelope[] envelopes = new IEnvelope[MaxCountEnvelope];
+
+            double? length;
+            double? height;
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Edit values for envelope number " + (i + 1) + ": ");
+                length = ConsoleMenu.GetDoubleValue("Length: ");
+                height = ConsoleMenu.GetDoubleValue("Height: ");
+                if(length == null)
+                {
+                    Console.WriteLine("Length is not valid");
+                    continue;
+                }
+                if (height == null)
+                {
+                    Console.WriteLine("Height is not valid");
+                    continue;
+                }
+                envelopes[i] = new RectangularEnvelope((double)length, (double)height);
+                i++;
+                if(i < MaxCountEnvelope)
+                {
+                    continue;
+                }
+                else
+                {
+
+                    if (envelopes[0].DoesFits(envelopes[1]))
+                    {
+                        Console.WriteLine("The second envelope will fit into the first");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The second envelope will not fit into the first");
+                    }
+                }
+
+                i = 0;
+                if (!ConsoleMenu.GetBoolValue("Do you want to continue?", new string[] { "Y", "YES" }))
+                {
+                    break;
+                }
+            } while (true);
+        }
+
+    }
+}
+ 
